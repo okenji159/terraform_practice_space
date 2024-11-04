3bdd7775652e04ff67a684c5629c04236dc8e5b9
okenji159 <165344166+okenji159@users.noreply.github.com>
2024-11-04 12:28:21 +0900

    instance_typeを変数で表記

--- instance_plactice/main.tf
+++ instance_plactice/main.tf
@@ -1,12 +1,20 @@
+#instance_typeの変数化
+variable "example_instance_type" {
+    default = "t3.micro"
+}
+
 resource "aws_instance" "test" {
     ami             =   "ami-0c3fd0f5d33134a76"
-    instance_type   =   "t3.micro"
+    instance_type   =   var.example_instance_type
 
+    /*
+    #apacheのインストール
     user_data = <<EOF
         #!/bin/bash
         yum install -y httpd
         systemctl start httpd.service
     EOF
+    */
 
     tags = {
         Name = "example"
