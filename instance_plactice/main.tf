ff13f201934d951cfc7625930173fad21f1b03a8
okenji159 <165344166+okenji159@users.noreply.github.com>
2024-11-04 12:32:57 +0900

    local変数の使用

--- instance_plactice/main.tf
+++ instance_plactice/main.tf
@@ -1,11 +1,18 @@
+/*
 #instance_typeの変数化
 variable "example_instance_type" {
     default = "t3.micro"
 }
+*/
+
+#locals変数でのinstance_typeの変数化
+locals {
+    example_instance_type = "t3.micro"
+}
 
 resource "aws_instance" "test" {
     ami             =   "ami-0c3fd0f5d33134a76"
-    instance_type   =   var.example_instance_type
+    instance_type   =   local.example_instance_type
 
     /*
     #apacheのインストール
