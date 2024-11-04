803a3ec5219092f3921171d39e5fdba742a3095a
okenji159 <165344166+okenji159@users.noreply.github.com>
2024-11-04 13:46:21 +0900

    apacheのインストール及びsecurity_groupの設定

--- instance_plactice/main.tf
+++ instance_plactice/main.tf
@@ -10,20 +10,41 @@ locals {
     example_instance_type = "t3.micro"
 }
 
+/*
+#datasourceで最新のamiを取得
+data "aws_ami" "recent_ami_id" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amazon2-ami-hvm-2.0.????????-x86_64-gp2"]
+    }
+
+    filter {
+        name = "state"
+        values = ["available"]
+    }
+}
+*/
+
 resource "aws_instance" "test" {
     ami             =   "ami-0c3fd0f5d33134a76"
     instance_type   =   local.example_instance_type
+    vpc_security_group_ids = [aws_security_group.for_ec2.id]
 
-    /*
     #apacheのインストール
     user_data = <<EOF
         #!/bin/bash
         yum install -y httpd
         systemctl start httpd.service
     EOF
-    */
 
     tags = {
         Name = "example"
     }
+}
+
+output "example_instance_id" {
+    value = aws_instance.test.public_dns
 }
---
+++ instance_plactice/security_group.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "for_ec2" {
+    name = "for-ec2"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
